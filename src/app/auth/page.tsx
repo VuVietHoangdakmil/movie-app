5cd8ba2de00002a941e39c9d9a6fad59df2a2fa8
VuVietHoangdakmil <vuviethoangdakmil@gmail.com>
2024-12-25 13:50:28 +0000

    update

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,12 @@
 "use client";
-
+import { usePathname } from 'next/navigation'
 import { useRouter } from "next/navigation";
 
 export default function Login() {
+  console.log("process.env.NODE_ENV",process.env.NEXT_PUBLIC_BASE_URL)
   const router = useRouter();
+  const path = usePathname();
+  console.log("path",path)
   const fecthToken = async () => {
     try {
       await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/setCookiesToken`);
